0660533eb781497964b34da4e89d07af536b244d
begueradj <begueradj.bill@gmail.com>
2019-11-29 18:54:41 +0200

    Try demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,22 @@
 <template>
   <v-container>
     <ChildPage :id="Number(1)" :state="Number(1)" />
+    <draggable v-model="myArray" @start="drag=true" @end="drag=false" group="people">
+      <div v-for="element in myArray" :key="element.id">
+        {{ element.name }}
+      </div>
+    </draggable>
   </v-container>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import ChildPage from '@/components/ChildPage.vue'
+
 export default {
   components: {
-    ChildPage
+    ChildPage,
+    draggable
   }
 }
 </script>
